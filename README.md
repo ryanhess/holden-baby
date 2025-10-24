71606903d8f5432c889e92e89406c744744e3e57
ryanhess <hess29@gmail.com>
2025-10-24 14:00:40 -0400

    basic timer structure and the start/stop button populates the list

--- README.md
+++ README.md
@@ -1,40 +1,11 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/pages/api-reference/create-next-app).
+Goals Oct 23:
 
-## Getting Started
+Contraction timer with a start/stop button that doesn't scroll and stays at one spot on the screen.
+-The button says START when no contraction is being recorded
+-When pressed, button changes to say STOP and a slider appears for the intensity.
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/pages/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn-pages-router) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/pages/building-your-application/deploying) for more details.
+List of contractions showing the:
+-time the contraction started
+-duration
+-intensity
+-interval from start of last contraction if any.
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,100 @@
-export default function Home() {
-  return;
+import { useState } from "react";
+
+/*
+State consists of:
+user hits Start to start contraction. Then we are "in" a contraction.
+-The start button turns to a Stop button,
+-a slider appears below to rate the intensity 1-10.
+-a timer appears to time how long the current contraction lasts.
+
+User hits stop, we are "out" of a contraction.
+The list of previous contractions populates. Each item:
+-the time it started
+-the duration
+-The intensity
+
+Alternating between each contraction in the list is an interval:
+-The time since the start of the last contraction (cadence)
+-(smaller font) the "rest time," or the time BETWEEN contractions
+
+
+*/
+
+export default function ContractionTimer() {
+  const [inContraction, setInContraction] = useState(false);
+  const [contractionHistory, setContractionHistory] = useState([]);
+  const [testInc, setTestInc] = useState(0);
+
+  function handleStartStop(startTime = null, duration = null, intensity = null) {
+    if (inContraction) {
+      setInContraction(false);
+      setTestInc(testInc + 1);
+
+      const newCtxHist = [...contractionHistory];
+      newCtxHist.push({
+        startTime: startTime,
+        duration: duration,
+        intensity: intensity
+      });
+      setContractionHistory(newCtxHist);
+    } else {
+      setInContraction(true);
+    }
+  }
+
+  return (
+    <div>
+      <StartStopContainer inContraction={inContraction} onButtonPress={handleStartStop} />
+      <ContractionList ctxList={contractionHistory} />
+    </div>
+  );
+}
+
+function StartStopContainer({ inContraction, onButtonPress }) {
+  if (inContraction) {
+    return <NewContractionUI handleStopCtx={onButtonPress} />;
+  } else {
+    return <StartButton handleStartCtx={onButtonPress} />;
+  }
+}
+
+function NewContractionUI({ handleStopCtx }) {
+  const startTime = '12:37pm';
+  const duration = 10;
+  const intensity = 3;
+  return <button onClick={() => handleStopCtx(startTime, duration, intensity)}>Stop</button>
+}
+
+function StartButton({ handleStartCtx }) {
+  return <button onClick={handleStartCtx}>Start</button>
+}
+
+function ContractionList({ ctxList }) {
+  let interval = null;
+  return (
+    <ul>
+      {ctxList.map((ctx, i, ctxHist) => {
+        if (i > 0) {
+          const prevCtx = ctxHist[i - 1];
+          interval =
+            <li>
+              <p>Contraction Cadence: {ctx.startTime - prevCtx.startTime}</p>
+              <p>Rest Time: {ctx.startTime - (prevCtx.startTime + prevCtx.duration)}</p>
+            </li>;
+        }
+        return (
+          <>
+            {interval}
+            <li> Contraction
+              <ul>
+                <li>Start Time: {ctx.startTime}</li>
+                <li>Duration: {ctx.duration}</li>
+                <li>Intensity: {ctx.intensity}</li>
+              </ul>
+            </li>
+          </>
+        );
+      })}
+    </ul >
+  );
 }
